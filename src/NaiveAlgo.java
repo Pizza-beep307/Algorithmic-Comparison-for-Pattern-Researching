d47c201c0b44c7134f0bed898974c34783b929a5
Pizza-beep307 <younes.chelda@gmail.com>
2025-12-19 11:18:29 +0100

    Conditions added in NaiveAlgo for all possible error, and more error test added

--- src/NaiveAlgo.java
+++ src/NaiveAlgo.java
@@ -16,31 +16,51 @@ class NaiveAlgo {
      */
 	ArrayList<Integer> naiveAlgo ( ArrayList<Character> text, String pattern ) {
 		cpt = 0; 
-		ArrayList<Integer> indices = new ArrayList<Integer>(); //2 op
-		int range = text.size() - pattern.length() + 1;
-		// 1 op		//1 op 		// 1 op				//1op
-		boolean findPattern;//1 op
-		int j; // 1 op
-		
-		for (int i = 0; i < range; i++) {
-			//2 op      // 1 op		// 2 op 
-			findPattern = true; // 1op
-			j = 0; // 1 op
-			while (j < pattern.length() && findPattern) {
-				   // j op					j op
-				cpt ++;
-				if (text.get(i + j) != pattern.charAt(j)) {
-					//
-					findPattern= false; // 1 op
+		ArrayList<Integer> indices = null;
+		if (text != null) {
+			if (text.size() > 0) {
+				if (pattern != null) {
+					if (pattern.length() > 0) {
+						if (pattern.length() <= text.size()) {
+							indices = new ArrayList<Integer>(); //2 op
+							int range = text.size() - pattern.length() + 1;
+							// 1 op		//1 op 		// 1 op		       // 1 op
+							boolean findPattern;//1 op
+							int j; // 1 op
+							
+							for (int i = 0; i < range; i++) {
+								//2 op      // 1 op		// 2 op 
+								findPattern = true; // 1op
+								j = 0; // 1 op
+								while (j < pattern.length() && findPattern) {
+									   // j op					j op
+									cpt ++;
+									if (text.get(i + j) != pattern.charAt(j)) {
+										//
+										findPattern= false; // 1 op
+									}
+									j ++; // 1 op
+								}
+								
+								if (findPattern) { // 1 op
+									indices.add(i + 1); // 1 op //respect de la convention
+								}
+							}
+						} else {
+							System.err.println("naiveAlgo(): Error: pattern is longer than the text");
+						}	
+					} else {
+						System.err.println("naiveAlgo(): Error: pattern is empty");
+					} 
+				} else {
+					System.err.println("naiveAlgo(): Error: pattern is null");
 				}
-				j ++; // 1 op
+			} else {
+				System.err.println("naiveAlgo(): Error: text is empty");
 			}
-			
-			if (findPattern) { // 1 op
-				indices.add(i + 1); // 1 op //respect de la convention
-			}
+		} else {
+			System.err.println("naiveAlgo(): Error: text is null");
 		}
-		
 		return indices;
 	}
 	/**
@@ -70,45 +90,73 @@ class NaiveAlgo {
         testCasNaiveAlgo(text, pattern, new ArrayList<>()); 
         
 
-        System.out.println("Test : Limit Case : Pattern at the very end");
-        text = new ArrayList<>(Arrays.asList('h', 'e', 'l', 'l', 'o'));
-        pattern = "lo";
-        testCasNaiveAlgo(text, pattern, new ArrayList<>(Arrays.asList(4)));
-        
-
-        System.out.println("Test : Limit Case : Pattern at the beginning");
-        text = new ArrayList<>(Arrays.asList('P', 'A', 'T', 'T', 'E', 'R', 'N'));
-        pattern = "PA";
-        testCasNaiveAlgo(text, pattern, new ArrayList<>(Arrays.asList(1)));
-        
-
         System.out.println("Test : Limit Case : Pattern as long as the text");
         text = new ArrayList<>(Arrays.asList('T', 'E', 'X', 'T'));
         pattern = "TEXT";
         testCasNaiveAlgo(text, pattern, new ArrayList<>(Arrays.asList(1)));
+            
+        
+        System.out.println("Test : Error Case : Pattern does not exist");
+        text = new ArrayList<>(Arrays.asList('T', 'E', 'X', 'T'));
+        pattern = null;
+        testCasNaiveAlgo(text, pattern, null);
+        
+        
+        System.out.println("Test : Error Case : Pattern is empty");
+        text = new ArrayList<>(Arrays.asList('T', 'E', 'X', 'T'));
+        pattern = "";
+        testCasNaiveAlgo(text, pattern, null);
+        
+        
+        System.out.println("Test : Error Case : Pattern longer than the text");
+        text = new ArrayList<>(Arrays.asList('T', 'E', 'X', 'T'));
+        pattern = "TEXTURE";
+        testCasNaiveAlgo(text, pattern, null);
+        
+        
+        System.out.println("Test : Error Case : Text does not exist");
+        text = null;
+        pattern = "srchNull";
+        testCasNaiveAlgo(text, pattern, null);
+        
+        
+        System.out.println("Test : Error Case : Text is empty");
+        text = new ArrayList<>();
+        pattern = "srchNull";
+        testCasNaiveAlgo(text, pattern, null);
+           
+        
     }
 	
     /**
-     * Test un cas particulier de naiveAlgo() en comparant le résultat obtenu avec un résultat attendu. 
      * Tests a specific case of naiveAlgo() by comparing the obtained result with an expected result. (Corresponds to testCasAlgo)
      * @param text - The text (ArrayList<Character>)
      * @param pattern - The pattern (String)
      * @param expected - The list of expected indices, now strictly an ArrayList<Integer>	
      */
     void testCasNaiveAlgo(ArrayList<Character> text, String pattern, ArrayList<Integer> expected) { 
-        
         ArrayList<Integer> resObtenu = naiveAlgo(text, pattern);
-        boolean succes = resObtenu.equals(expected);
-        
-        if (succes) {
-			System.out.println(resObtenu);
-            System.out.println("Test successful for pattern \"" + pattern + "\" in " + text.toString());
-        } else {
-       
-            System.err.println("Test failed for pattern \"" + pattern + "\" in " + text.toString());
-        }
-        System.out.println("");
-    }
+        if (expected != null) { 
+			boolean succes = resObtenu.equals(expected);
+			
+			if (succes) {
+				System.out.println(resObtenu);
+				System.out.println("Test successful for pattern \"" + pattern + "\" in " + text.toString() + "\n");
+			} else {
+		   
+				System.err.println("Test failed for pattern \"" + pattern + "\" in " + text.toString() + "\n");
+			}
+			
+		} else {
+			if (resObtenu == expected) {
+				System.out.println("Test successful\n");
+			} else {
+				System.err.println("Test failed\n");
+			}
+		}
+			
+	}
+		
 	
 	void testNaiveAlgoEfficiency() {
 		
